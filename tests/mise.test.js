b068a4583531df35638d001e9709061efdb38a0e
dsul <dsul@users.noreply.github.com>
2020-10-11 23:31:18 -0400

    Add back empty object constructor argument test

--- tests/mise.test.js
+++ tests/mise.test.js
@@ -2,6 +2,7 @@ const Mise = require('../src/Mise')
 
 test('the constructor argument must be a function', () => {
   const invalidConstructors = []
+  invalidConstructors.push(() => new Mise({}))
   invalidConstructors.push(() => new Mise(''))
   invalidConstructors.push(() => new Mise(1))
   invalidConstructors.push(() => new Mise(true))
