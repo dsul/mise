ebeb7088a92e9a63f212c5dcf23e1f7930d8fc90
dsul <dsul@users.noreply.github.com>
2020-10-14 00:56:50 -0400

    Experiment with a state machine implementation as an exercise

--- src/Mise.js
+++ src/Mise.js
@@ -3,7 +3,45 @@ class Mise {
     if (!(executor instanceof Function)) {
       throw new TypeError('The constructor argument must be a function.')
     }
+    const resolve = (value) => stateMachine.dispatch('resolve', value)
+    const reject = () => {}
+    executor(resolve, reject)
+  }
+
+  then(onFulfilled, onRejected) {
+    stateMachine.dispatch('then', onFulfilled, onRejected)
+  }
+}
+
+const stateMachine = {
+  state: 'pending',
+  currentValue: undefined,
+  dispatch(actionName, ...args) {
+    const action = this.transitions[this.state][actionName]
+    action.apply(stateMachine, args)
+  },
+  changeState(state) {
+    this.state = state
+  },
+  transitions: {
+    'pending': {
+      resolve: function (value) {
+        this.changeState('fulfilled')
+        this.currentValue = value
+      }
+    },
+    'fulfilled': {
+      then: function (onFulfilled, _) {
+        onFulfilled(this.currentValue)
+      }
+    },
+    'rejected': {
+      then: function (_, onRejected) {
+        onRejected(this.currentValue)
+      }
+    }
   }
 }
 
 module.exports = Mise
+
